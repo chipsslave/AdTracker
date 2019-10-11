ff60bddb01c54efb711e1258a523238de1663d37
Martin <mselvenis@gmail.com>
2019-10-11 20:17:02 +0100

    implemented method to read page html content when response code is above 400

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,7 +1,5 @@
 package com;
 
-import com.model.Ad;
-import com.parser.jsoup.AutoPlius.page.inside.AutoPliusIndividualAdPageImpl;
 import com.storage.db.DataBaseFactory;
 import com.storage.db.GsonDataBaseImpl;
 import com.util.UrlContentReader;
@@ -10,21 +8,28 @@ import org.jsoup.nodes.Document;
 
 public class Main {
 
-    // https://en.autoplius.lt/ads/volkswagen-touareg-4-1-l-suv-off-road-2010-diesel-9605847.html
+    String url = "https://en.autoplius.lt/ads/volkswagen-touareg-4-1-l-suv-off-road-2010-diesel-9605847.html";
+
 
     public static void main(String[] args){
         Main main = new Main();
         GsonDataBaseImpl localJsonStorage = DataBaseFactory.getDataBaseInstance();
 
-        while (localJsonStorage.getAdToUpdate() != null) {
-            Ad ad = localJsonStorage.getAdToUpdate();
-            System.out.println(ad.getAdUrl());
-            Document pageInHtml = UrlContentReader.readContentInJsoupDocument(ad.getAdUrl());
-            AutoPliusIndividualAdPageImpl<GsonDataBaseImpl> autoPliusIndividualAdPage = new AutoPliusIndividualAdPageImpl<>(pageInHtml, localJsonStorage, ad);
-            autoPliusIndividualAdPage.checkAdInWebsite();
-        }
+        String url = "https://en.autoplius.lt/ads/volkswagen-touareg-4-1-l-suv-off-road-2010-diesel-9605847.html";
+        //url = "https://www.journaldev.com/7148/java-httpurlconnection-example-java-http-request-get-post";
 
-        localJsonStorage.commit();
+        Document str = UrlContentReader.readContentInJsoupDocument(url);
+        System.out.println(str);
+
+//        while (localJsonStorage.getAdToUpdate() != null) {
+//            Ad ad = localJsonStorage.getAdToUpdate();
+//            System.out.println(ad.getAdUrl());
+//            Document pageInHtml = UrlContentReader.readContentInJsoupDocument(ad.getAdUrl());
+//            AutoPliusIndividualAdPageImpl<GsonDataBaseImpl> autoPliusIndividualAdPage = new AutoPliusIndividualAdPageImpl<>(pageInHtml, localJsonStorage, ad);
+//            autoPliusIndividualAdPage.checkAdInWebsite();
+//        }
+//
+//        localJsonStorage.commit();
 
 
     }
--- src/main/java/com/util/UrlContentReader.java
+++ src/main/java/com/util/UrlContentReader.java
@@ -6,25 +6,22 @@ import org.jsoup.nodes.Document;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class UrlContentReader {
     /**
      * Do GET request to target host
      *
-     * @param target
-     *            - like "https://www.delfi.lt"
+     * @param target - like "https://www.delfi.lt"
      * @return page content
      */
-    public static StringBuilder readContent(String target) {
+    private static StringBuilder readContent(String target) {
 
         StringBuilder builder = new StringBuilder();
         try {
-            // TODO does not work with response codes above 400
-
-
             BufferedReader in = new BufferedReader(new InputStreamReader((new URL(target)).openStream()));
-            in.lines().forEachOrdered(ln -> builder.append(ln + "\n"));
+            in.lines().forEachOrdered(ln -> builder.append(ln).append("\n"));
             in.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -32,8 +29,39 @@ public class UrlContentReader {
         return builder;
     }
 
-    public static Document readContentInJsoupDocument(String target){
-        StringBuilder stingBuilderHtml = readContent(target);
+    public static Document readContentInJsoupDocument(String target) {
+        StringBuilder stingBuilderHtml = readHtmlContent(target);
         return Jsoup.parse(stingBuilderHtml.toString());
+    }
+
+    private static StringBuilder readHtmlContent(String target) {
+        StringBuilder response = new StringBuilder();
+        try {
+            URL obj = new URL(target);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+            if (con.getResponseCode() < 400) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+            } else {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+                String inputLine;
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return response;
     }
 }
