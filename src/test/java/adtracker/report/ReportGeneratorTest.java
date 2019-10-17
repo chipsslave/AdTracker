2ece738a475cca3d8de90e0805a6c455694c133e
Martin <mselvenis@gmail.com>
2019-10-17 14:02:16 +0100

    test rouserces path

--- src/test/java/adtracker/report/ReportGeneratorTest.java
+++ src/test/java/adtracker/report/ReportGeneratorTest.java
@@ -3,37 +3,69 @@ package adtracker.report;
 import adtracker.model.Ad;
 import adtracker.report.comparator.AdsListedComparator;
 import adtracker.report.reports.r01.R01ReportItem;
-import adtracker.storage.db.DataBaseFactory;
-import adtracker.storage.db.GsonDataBaseImpl;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 import static org.junit.Assert.assertEquals;
 
 public class ReportGeneratorTest {
 
-    private final GsonDataBaseImpl adsDataBase = DataBaseFactory.getDataBaseInstance("src/test/java/resource/TESTS_DATA.json");
+    private List<Ad> adList = new ArrayList<>();
 
     private ReportGenerator reportGenerator = new ReportGenerator();
 
-    private List<?> r01ReportItemsAscending = reportGenerator.getReport(ReportList.R01, adsDataBase.getAll(), new AdsListedComparator(true));
-    private List<?> r01ReportItemsDescending = reportGenerator.getReport(ReportList.R01, adsDataBase.getAll(), new AdsListedComparator(false));
+    private List<?> r01ReportItemsAscending = null;
+    private List<?> r01ReportItemsDescending = null;
 
-    private List<R01ReportItem> r01ReportItemsDescendingSuccess = Arrays.asList(
-            new R01ReportItem("Ford", 3, 0, 0.0),
-            new R01ReportItem("Volvo", 2, 0, 0.0),
-            new R01ReportItem("Subaru", 1, 0, 0.0),
-            new R01ReportItem("Nissan", 1, 0, 0.0)
-    );
+    private List<R01ReportItem> r01ReportItemsDescendingSuccess = null;
 
-    private List<R01ReportItem> r01ReportItemsAscendingSuccess = Arrays.asList(
-            new R01ReportItem("Nissan", 1, 0, 0.0),
-            new R01ReportItem("Subaru", 1, 0, 0.0),
-            new R01ReportItem("Volvo", 2, 0, 0.0),
-            new R01ReportItem("Ford", 3, 0, 0.0)
-    );
+    private List<R01ReportItem> r01ReportItemsAscendingSuccess = null;
+
+    @Before
+    public void loadData() {
+        Gson GSON = new Gson();
+        File file = new File(Objects.requireNonNull(getClass().getClassLoader().getResource("TESTS_DATA.json")).getFile());
+
+
+        try {
+            Reader reader = new FileReader(file);
+            Type listType = new TypeToken<List<Ad>>() {
+            }.getType();
+            adList = GSON.fromJson(reader, listType);
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        r01ReportItemsAscending = reportGenerator.getReport(ReportList.R01, adList, new AdsListedComparator(true));
+        r01ReportItemsDescending = reportGenerator.getReport(ReportList.R01, adList, new AdsListedComparator(false));
+
+        r01ReportItemsDescendingSuccess = Arrays.asList(
+                new R01ReportItem("Ford", 3, 0, 0.0),
+                new R01ReportItem("Volvo", 2, 0, 0.0),
+                new R01ReportItem("Subaru", 1, 0, 0.0),
+                new R01ReportItem("Nissan", 1, 0, 0.0)
+        );
+
+        r01ReportItemsAscendingSuccess = Arrays.asList(
+                new R01ReportItem("Nissan", 1, 0, 0.0),
+                new R01ReportItem("Subaru", 1, 0, 0.0),
+                new R01ReportItem("Volvo", 2, 0, 0.0),
+                new R01ReportItem("Ford", 3, 0, 0.0)
+        );
+    }
 
     @Test
     public void checkIfReportIsAsExpected() {
@@ -56,14 +88,14 @@ public class ReportGeneratorTest {
 
     @Test(expected = NullPointerException.class)
     public void checkIfNullValueThrowsExceptionAscending() {
-        List<Ad> r01ReportItemsFail = adsDataBase.getAll();
+        List<Ad> r01ReportItemsFail = adList;
         r01ReportItemsFail.add(null);
         reportGenerator.getReport(ReportList.R01, r01ReportItemsFail, new AdsListedComparator(true));
     }
 
     @Test(expected = NullPointerException.class)
     public void checkIfNullValueThrowsExceptionDescending() {
-        List<Ad> r01ReportItemsFail = adsDataBase.getAll();
+        List<Ad> r01ReportItemsFail = adList;
         r01ReportItemsFail.add(null);
         reportGenerator.getReport(ReportList.R01, r01ReportItemsFail, new AdsListedComparator(false));
     }
